148d8cdb66b1b5de13c1acab726b2b5c880cbc76
IonZero <kayonosukses@gmail.com>
2020-10-02 14:46:04 +0700

    Initial commit

---
+++ app/src/main/java/www/vsga/karyono/MainActivity.java
@@ -0,0 +1,14 @@
+package www.vsga.karyono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
